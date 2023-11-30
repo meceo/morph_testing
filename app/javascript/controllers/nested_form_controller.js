e9edb2b91b4aca3ce52b92ddd429c6830d823e04
Mateusz Juraszek <mateusz@webtigers.eu>
2023-11-30 10:21:46 +0100

    further cut the noise of external libs

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,4 +1,21 @@
-import NestedForm from 'stimulus-rails-nested-form'
+import { Controller } from "@hotwired/stimulus"
 
-export default class extends NestedForm {
+export default class extends Controller {
+  static targets = ["target", "template"]
+  static values = {
+    wrapperSelector: {
+      type: String,
+      default: ".nested-form-wrapper"
+    }
+  }
+
+  add(e) {
+    e.preventDefault()
+
+    const content = this.templateTarget.innerHTML.replace(
+      /NEW_RECORD/g,
+      new Date().getTime().toString()
+    )
+    this.targetTarget.insertAdjacentHTML("beforebegin", content)
+  }
 }
